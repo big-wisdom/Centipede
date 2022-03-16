3d2967aa6f804e197a2d81f28867791bc67cf503
Eli Hermann <Eli_Hermann@hotmail.com>
2022-03-15 22:00:49 -0600

    basic menu code integrated

---
+++ Centipede/Assignment3.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace CS5410
+{
+    public class Assignment3 : Game
+    {
+        private GraphicsDeviceManager m_graphics;
+        private SpriteBatch m_spriteBatch;
+
+        private IGameState m_currentState;
+        private GameStateEnum m_nextStateEnum = GameStateEnum.MainMenu;
+        private Dictionary<GameStateEnum, IGameState> m_states;
+
+        public Assignment3()
+        {
+            m_graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            m_states = new Dictionary<GameStateEnum, IGameState>();
+            m_states.Add(GameStateEnum.MainMenu, new MainMenuView());
+
+            // We are starting with the main menu
+            m_currentState = m_states[GameStateEnum.MainMenu];
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            m_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            foreach (var item in m_states)
+            {
+                item.Value.initialize(this.GraphicsDevice, m_graphics);
+                item.Value.loadContent(this.Content);
+            }
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            m_nextStateEnum = m_currentState.processInput(gameTime);
+            // Special case for exiting the game
+            if (m_nextStateEnum == GameStateEnum.Exit)
+            {
+                Exit();
+            }
+
+            m_currentState.update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            m_currentState.render(gameTime);
+
+            m_currentState = m_states[m_nextStateEnum];
+
+            base.Draw(gameTime);
+        }
+    }
+}
